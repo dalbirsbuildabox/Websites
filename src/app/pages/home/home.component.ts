6afc5eb1df1407e7a659743540759bb886997866
farooqghaznavi <ghaznavi699@gmai.com>
2025-03-12 01:20:45 +0500

    changes are implemented learn more button

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AboutUsComponent } from '../about-us/about-us.component';
 import { ServicesComponent } from '../services/services.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [AboutUsComponent, ServicesComponent],
+  imports: [AboutUsComponent, ServicesComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
